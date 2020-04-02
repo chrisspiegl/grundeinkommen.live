c281c401e43248453085111262bd00866778d1db
Chris Spiegl <chris@chrisspiegl.com>
2020-04-02 13:51:04 +0200

    Reduce code duplication by generating accumulated data and charts in one function and just work with database keys to input. Added cache to the data generation to reduce load time (just a in memory cache with `node-cache`). This reduced the page render time from 1s to almost 0 once the first request is rendered. Cache has a TTL of 5 minutes.

--- server/router/home.js
+++ server/router/home.js
@@ -9,11 +9,15 @@ const error = debug(`${config.slug}:router:home:error`)
 
 const express = require('express')
 const numeral = require('numeral')
+const _ = require('lodash')
 const moment = require('moment-timezone')
 const sequelize = require('sequelize')
 
 const models = require('../../database/models')
 const middleware = require('../middleware')
+
+const CacheService = require('../cache.service');
+const cache = new CacheService(60 * 5); // 5min ttl / Create a new cache service instance
 
 module.exports = () => {
   const router = express.Router()
@@ -22,210 +26,17 @@ module.exports = () => {
       bodyClasses: 'pageHome'
     }
 
-
-    /**
-    changeOrg =======================================
-    */
-    response.changeOrgDe = await models.ValuesInt.findAll({
-      where: {
-        key: 'change-org-bge-de'
-      },
-      order: [['createdAt', 'ASC']]
-    })
-
-    response.changeOrgDeCurrent = response.changeOrgDe.slice(-1)[0]
-
-    const chartChangeOrg = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    const chartChangeOrgDay = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    let hourLast = moment().add(1, 'hour').startOf('hour')
-    let dayLast = moment().add(1, 'day').startOf('day')
-    for (let change of response.changeOrgDe) {
-      const hourCurrent = moment(change.createdAt).startOf('hour')
-      const dayCurrent = moment(change.createdAt).startOf('day')
-
-      if (hourLast.isSame(hourCurrent)) {
-      } else {
-        hourLast = hourCurrent
-        chartChangeOrg.labels.push(hourLast.tz('Europe/Berlin').format('YYYY-MM-DD HH:mm'))
-        chartChangeOrg.values.push(numeral(change.value).format('0.0'))
-        chartChangeOrg.valuesRaw.push(change.value)
-      }
-      if (dayLast.isSame(dayCurrent)) {
-      } else {
-        dayLast = dayCurrent
-        chartChangeOrgDay.labels.push(dayLast.tz('Europe/Berlin').format('YYYY-MM-DD'))
-        chartChangeOrgDay.values.push(numeral(change.value).format('0.0'))
-        chartChangeOrgDay.valuesRaw.push(change.value)
-      }
-    }
-
-    response.chartChangeOrg = chartChangeOrg
-    response.chartChangeOrgDay = chartChangeOrgDay
-
-    let hourLast1 = moment().subtract(2, 'hours').startOf('hour')
-    let hourLast24 = moment().subtract(24, 'hours').startOf('hour')
-    let changeOrg24h, changeOrg1h;
-    for (let change of response.changeOrgDe) {
-      if (!changeOrg1h && moment(change.createdAt).startOf('hour').isAfter(hourLast1)) {
-        changeOrg1h = change
-      }
-      if (!changeOrg24h && moment(change.createdAt).startOf('hour').isAfter(hourLast24)) {
-        changeOrg24h = change
-      }
-    }
-
-    response.changeOrg1h = (changeOrg1h) ? response.changeOrgDeCurrent.value - changeOrg1h.value : 0
-    response.changeOrg24h = (changeOrg24h) ? response.changeOrgDeCurrent.value - changeOrg24h.value : 0
-
-
-    /**
-    bundestag =======================================
-    */
-    response.bundestag = await models.ValuesInt.findAll({
-      where: {
-        key: 'bundestag-108191'
-      },
-      order: [['createdAt', 'ASC']]
-    })
-
-    response.bundestagCurrent = response.bundestag.slice(-1)[0]
-
-    const chartBundestag = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    const chartBundestagDay = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    hourLast = moment().add(1, 'hour').startOf('hour')
-    dayLast = moment().add(1, 'day').startOf('day')
-    for (let change of response.bundestag) {
-      const hourCurrent = moment(change.createdAt).startOf('hour')
-      const dayCurrent = moment(change.createdAt).startOf('day')
-
-      if (hourLast.isSame(hourCurrent)) {
-      } else {
-        hourLast = hourCurrent
-        chartBundestag.labels.push(hourLast.tz('Europe/Berlin').format('YYYY-MM-DD HH:mm'))
-        chartBundestag.values.push(numeral(change.value).format('0.0'))
-        chartBundestag.valuesRaw.push(change.value)
-      }
-      if (dayLast.isSame(dayCurrent)) {
-      } else {
-        dayLast = dayCurrent
-        chartBundestagDay.labels.push(dayLast.tz('Europe/Berlin').format('YYYY-MM-DD'))
-        chartBundestagDay.values.push(numeral(change.value).format('0.0'))
-        chartBundestagDay.valuesRaw.push(change.value)
-      }
-    }
-
-    response.chartBundestag = chartBundestag
-    response.chartBundestagDay = chartBundestagDay
-
-    hourLast1 = moment().subtract(2, 'hours').startOf('hour')
-    hourLast24 = moment().subtract(24, 'hours').startOf('hour')
-    let bundestag24h, bundestag1h;
-    for (let change of response.bundestag) {
-      if (!bundestag1h && moment(change.createdAt).startOf('hour').isAfter(hourLast1)) {
-        bundestag1h = change
-      }
-      if (!bundestag24h && moment(change.createdAt).startOf('hour').isAfter(hourLast24)) {
-        bundestag24h = change
-      }
-    }
-
-    response.bundestag1h = (bundestag1h) ? response.bundestagCurrent.value - bundestag1h.value : 0
-    response.bundestag24h = (bundestag24h) ? response.bundestagCurrent.value - bundestag24h.value : 0
-
-
-
-    /**
-    youmove =======================================
-    */
-
-    response.youMove = await models.ValuesInt.findAll({
-      where: {
-        key: 'youMove-eu-grundeinkommen'
-      },
-      order: [['createdAt', 'ASC']]
-    })
-
-    response.youMoveCurrent = response.youMove.slice(-1)[0]
-
-    const chartYouMove = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    const chartYouMoveDay = {
-      labels: [],
-      values: [],
-      valuesRaw: []
-    }
-    hourLast = moment().add(1, 'hour').startOf('hour')
-    dayLast = moment().add(1, 'day').startOf('day')
-    for (let change of response.youMove) {
-      const hourCurrent = moment(change.createdAt).startOf('hour')
-      const dayCurrent = moment(change.createdAt).startOf('day')
-      if (hourLast.isSame(hourCurrent)) {
-      } else {
-        hourLast = hourCurrent
-        chartYouMove.labels.push(hourLast.tz('Europe/Berlin').format('YYYY-MM-DD HH:mm'))
-        chartYouMove.values.push(numeral(change.value).format('0.0'))
-        chartYouMove.valuesRaw.push(change.value)
-      }
-      if (dayLast.isSame(dayCurrent)) {
-      } else {
-        dayLast = dayCurrent
-        chartYouMoveDay.labels.push(dayLast.tz('Europe/Berlin').format('YYYY-MM-DD'))
-        chartYouMoveDay.values.push(numeral(change.value).format('0.0'))
-        chartYouMoveDay.valuesRaw.push(change.value)
-      }
-    }
-
-    response.chartYouMove = chartYouMove
-    response.chartYouMoveDay = chartYouMoveDay
-
-    let youMove24h, youMove1h;
-    for (let change of response.youMove) {
-      if (!youMove1h && moment(change.createdAt).startOf('hour').isAfter(hourLast1)) {
-        youMove1h = change
-      }
-      if (!youMove24h && moment(change.createdAt).startOf('hour').isAfter(hourLast24)) {
-        youMove24h = change
-      }
-    }
-    response.youMove1h = (youMove1h) ? response.youMoveCurrent.value - youMove1h.value : 0
-    response.youMove24h = (youMove24h) ? response.youMoveCurrent.value - youMove24h.value : 0
-
+    response.changeorg = await cache.get('change-org-bge-de', () => chartGrouping('change-org-bge-de'))
+    response.bundestag = await cache.get('bundestag-108191', () => chartGrouping('bundestag-108191'))
+    response.youmove = await cache.get('youMove-eu-grundeinkommen', () => chartGrouping('youMove-eu-grundeinkommen'))
 
     /**
     meinbge =======================================
     */
-    response.meinbgeGrundeinkommen = await models.ValuesInt.findOne({
-      where: {
-        key: 'meinbge-de-grundeinkommen'
-      },
-      order: [['createdAt', 'DESC']]
-    })
 
-    response.meinbgeDonors = await models.ValuesInt.findOne({
-      where: {
-        key: 'meinbge-de-donors'
-      },
-      order: [['createdAt', 'DESC']]
-    })
+    response.meinbge = {}
+    response.meinbge.grundeinkommen = await cache.get('meinbge-de-grundeinkommen', () => chartGrouping('meinbge-de-grundeinkommen'))
+    response.meinbge.donors = await cache.get('meinbge-de-donors', () => chartGrouping('meinbge-de-donors'))
 
     return res.format({
       html: () => {
@@ -237,5 +48,62 @@ module.exports = () => {
     })
   }))
 
+
   return router
 }
+
+
+
+async function chartGrouping(key) {
+  const allValues = await models.ValuesInt.findAll({
+    where: {
+      key: key
+    },
+    order: [['createdAt', 'ASC']]
+  })
+
+  const current = allValues.slice(-1)[0]
+
+  const chartHourly = {
+    labels: [],
+    valuesRaw: []
+  }
+  const chartDaily = {
+    labels: [],
+    valuesRaw: []
+  }
+
+  // Grouped by Day Values
+  const groupedByDay = _.groupBy(allValues, (el) => moment(el.createdAt).startOf('day').format())
+  const maxByDay = _.map(groupedByDay, (el) => {
+    return _.maxBy(el, 'value')
+  })
+  for (let el of maxByDay) {
+    chartDaily.labels.push(moment(el.createdAt).tz('Europe/Berlin').startOf('day').format('YYYY-MM-DD'))
+    chartDaily.valuesRaw.push(el.value)
+  }
+
+  // Grouped by Hour Values
+  const groupedByHour = _.groupBy(allValues, (el) => moment(el.createdAt).startOf('hour').format())
+  const maxByHour = _.map(groupedByHour, (el) => {
+    return _.maxBy(el, 'value')
+  })
+
+  for (let el of maxByHour) {
+    chartHourly.labels.push(moment(el.createdAt).tz('Europe/Berlin').startOf('hour').format('YYYY-MM-DD  HH:mm'))
+    chartHourly.valuesRaw.push(el.value)
+  }
+
+  // Last 1 Hour Values
+  const last1h = Math.abs(_.nth(maxByHour, -1).value - _.nth(maxByHour, -2).value)
+
+  // Last 24 Hours Values
+  time24hoursago = moment(_.last(maxByHour).createdAt).startOf('hour').subtract(24, 'hours')
+  const val24hago = _.find(maxByHour, (el) => {
+    return moment(el.createdAt).tz('Europe/Berlin').startOf('hour').isAfter(time24hoursago)
+  })
+  const last24h = Math.abs(val24hago.value - _.nth(maxByDay, -1).value)
+
+  return {allValues, current, chartDaily, chartHourly, last1h, last24h}
+}
+
